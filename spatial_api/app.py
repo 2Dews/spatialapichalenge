c065ed64d9bc84750a0066e8d92645dcd04c34fb
Gus Martinka <gus.martinka@ihsmarkit.com>
2020-04-15 17:40:04 -0600

    Working tests and local SAM webapi

--- spatial_api/app.py
+++ spatial_api/app.py
@@ -1,11 +1,45 @@
 import awsgi
+
 from flask import (
     Flask,
     jsonify,
     request,
 )
+from shapely import (
+    geometry,
+    wkt
+)
+import json
+import geojson
+import shapely
 
-from spatial_api import spatial_library
+def geometry_string_to_geometry(geometry_string):
+    """
+    Creates a shapely geometry object from a geojson or wkt string.
+    """
+    try:
+        if geojson.loads(json.dumps(geometry_string)).is_valid:
+            return geometry.shape(geometry_string)
+    except:
+        print("Input is not valid json.")
+
+    try: 
+        wktGeometry = wkt.loads(geometry_string)
+        return wktGeometry
+    except:
+        raise Exception('Could not create geometry object from string: {0}'.format(geometry_string))
+
+
+def does_intersect(geometry_string_a, geometry_string_b):
+    """
+    Returns true when polygon_a spatially intersects polygon_b.
+    """   
+    geometry_a = geometry_string_to_geometry(geometry_string_a)
+    geometry_b = geometry_string_to_geometry(geometry_string_b)
+
+    #TODO Checks that the polygons have same spatial reference.
+
+    return geometry_a.intersects(geometry_b)
 
 app = Flask(__name__)
 
@@ -18,9 +52,11 @@ def server_status():
 def geometry_intersection():
     requstJson = request.get_json()
 
-    doesIntersect = spatial_library.does_intersect(requstJson['geometry_a'], requstJson['geometry_a'])
+    doesIntersect = does_intersect(requstJson['geometry_a'], requstJson['geometry_a'])
 
-    response = {'status': 'ok'}
+    response = {'status': 'ok',
+                'intersects': doesIntersect
+    }
 
     return jsonify(response)
 
--- test/test_spatial.py
+++ test/test_spatial.py
@@ -10,7 +10,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
-import spatial_api.spatial_library
+from spatial_api import app
 
 testdata = [
     ('POLYGON((-11648991.3615983 4921487.3851758,-11648463.8373444 4921488.17577299,-11648467.7337492 4922020.46492316,-11648994.3243665 4922018.95145425,-11648991.3615983 4921487.3851758))', 
@@ -24,22 +24,31 @@ testdata = [
     False)
 ]
 
+testGeoJson = [
+    (
+        {"type": "Polygon", "coordinates": [[[-11648991.3615983, 4921487.3851758], [-11648463.8373444, 4921488.17577299], [-11648467.7337492, 4922020.46492316], [-11648994.3243665, 4922018.95145425], [-11648991.3615983, 4921487.3851758]]]},
+        {"type": "Polygon", "coordinates": [[[-11648991.3615983, 4921487.3851758], [-11648463.8373444, 4921488.17577299], [-11648467.7337492, 4922020.46492316], [-11648994.3243665, 4922018.95145425], [-11648991.3615983, 4921487.3851758]]]},
+        True     
+    )
+
+]
+
 @pytest.mark.parametrize("polyA,polyB,expected", testdata)
 def test_valid_geojson_polygons_intersect(polyA, polyB, expected):
 
-    polyAJson = json.dumps(shapely.geometry.mapping(shapely.wkt.loads(polyA)))
-    polyBJson = json.dumps(shapely.geometry.mapping(shapely.wkt.loads(polyB)))
+    polyAJson = shapely.geometry.mapping(shapely.wkt.loads(polyA))
+    polyBJson = shapely.geometry.mapping(shapely.wkt.loads(polyB))
 
-    intersects = spatial_api.spatial_library.does_intersect(polyAJson, polyBJson)
+    intersects = app.does_intersect(polyAJson, polyBJson)
     assert intersects == expected
 
 
 @pytest.mark.parametrize("polyA,polyB,expected", testdata)
 def test_valid_wkt_polygons_intersect(polyA, polyB, expected):
-    intersects = spatial_api.spatial_library.does_intersect(polyA, polyB)
+    intersects = app.does_intersect(polyA, polyB)
     assert intersects == expected
 
 
 def test_invalid_string_polygon_intersection():
     with pytest.raises(Exception, match=r'.*Could not create geometry object from string.*'):
-        spatial_api.spatial_library.does_intersect('*', r'{}')
+        app.does_intersect('*', r'{}')
